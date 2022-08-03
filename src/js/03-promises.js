319dd3e745d64b10390a9770730bb7de86b7441f
gdutchak <galyna.dutchak99@gmail.com>
2022-08-04 00:33:26 +0300

    update promise 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,37 +5,36 @@ const refs = {
   button: document.querySelector("button")
 }
 
+function startPromise() {
+  const amount = refs.form["amount"].value;
+  let delayFrs = JSON.parse(refs.form["delay"].value);
+  let step = JSON.parse(refs.form["step"].value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delayFrs).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayFrs += step
+  }
+}
 
 function createPromise(position, delay) {
 
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay })
-    } else {
-      reject({ position, delay })
-    }
-  })
-}
 
-function startPromise() {
-  const amount = refs.form["amount"].value
-  let delayFrs = refs.form["delay"].value
-  let step = refs.form["step"].value
-  delayFrs = JSON.parse(delayFrs)
-  step = JSON.parse(step)
-  for (let i = 1, delay = delayFrs; i <= amount; i += 1, delay += step) {
-    console.log(delay);
     setTimeout(() => {
-      createPromise(i, delay).then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-        .catch(({ position, delay }) => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
     }, delay)
+  })
 
-  }
 }
 
 refs.button.addEventListener("click", (e) => {
